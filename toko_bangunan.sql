7b4bb50653a1dbd52b31f7c60a0625e4829ab47f
Muhammad Ardita Hilmi <141424736+hehe986@users.noreply.github.com>
2025-12-24 20:15:53 +0700

    Update toko_bangunan.sql

--- toko_bangunan.sql
+++ toko_bangunan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Des 2025 pada 12.26
+-- Waktu pembuatan: 24 Des 2025 pada 14.15
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -41,71 +41,6 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
 (1, 'admin', 'scrypt:32768:8:1$vXy... (Hash dibuat di app.py)'),
 (2, 'hilmi', 'pbkdf2:sha256:1000000$B6rGlK1CX7Jt5vGF$bdaac0f8e72759f7a63247a76cbfe55db6d1d28abec21527cd237d6d40132ab3');
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detail_pesanan`
---
-
-CREATE TABLE `detail_pesanan` (
-  `id_detail` int(11) NOT NULL,
-  `id_pesanan` int(11) DEFAULT NULL,
-  `id_produk` int(11) DEFAULT NULL,
-  `jumlah` int(11) DEFAULT NULL,
-  `harga_satuan` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pelanggan`
---
-
-CREATE TABLE `pelanggan` (
-  `id` int(11) NOT NULL,
-  `nama_lengkap` varchar(100) DEFAULT NULL,
-  `nomor_hp` varchar(15) DEFAULT NULL,
-  `alamat` text DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `pelanggan`
---
-
-INSERT INTO `pelanggan` (`id`, `nama_lengkap`, `nomor_hp`, `alamat`, `password`) VALUES
-(1, 'jule', '123456', 'jogja', 'scrypt:32768:8:1$wqzahKWxkWT8ygCd$9a84d1b2468e7c782b0e14ac065f9aa3ff1871950240c2fb7d7b833e44f1c99a55f308e87bf4388e13d01954c6f1cef25fbd44de385171d540145882f5188249');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `pesanan`
---
-
-CREATE TABLE `pesanan` (
-  `id_pesanan` int(11) NOT NULL,
-  `id_pelanggan` int(11) DEFAULT NULL,
-  `total_bayar` int(11) DEFAULT NULL,
-  `status` varchar(50) DEFAULT 'Diproses',
-  `notif_viewed` tinyint(1) DEFAULT 0,
-  `tanggal_pesanan` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `produk`
---
-
-CREATE TABLE `produk` (
-  `id_produk` int(11) NOT NULL,
-  `nama_produk` varchar(100) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
-  `stok` int(11) DEFAULT NULL,
-  `gambar` varchar(255) DEFAULT 'default.jpg',
-  `status` varchar(20) DEFAULT 'aktif'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Indexes for dumped tables
 --
@@ -118,34 +53,6 @@ ALTER TABLE `admin`
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- Indeks untuk tabel `detail_pesanan`
---
-ALTER TABLE `detail_pesanan`
-  ADD PRIMARY KEY (`id_detail`),
-  ADD KEY `id_pesanan` (`id_pesanan`),
-  ADD KEY `id_produk` (`id_produk`);
-
---
--- Indeks untuk tabel `pelanggan`
---
-ALTER TABLE `pelanggan`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nomor_hp` (`nomor_hp`);
-
---
--- Indeks untuk tabel `pesanan`
---
-ALTER TABLE `pesanan`
-  ADD PRIMARY KEY (`id_pesanan`),
-  ADD KEY `id_pelanggan` (`id_pelanggan`);
-
---
--- Indeks untuk tabel `produk`
---
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`);
-
---
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -154,47 +61,6 @@ ALTER TABLE `produk`
 --
 ALTER TABLE `admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT untuk tabel `detail_pesanan`
---
-ALTER TABLE `detail_pesanan`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `pelanggan`
---
-ALTER TABLE `pelanggan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `pesanan`
---
-ALTER TABLE `pesanan`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `produk`
---
-ALTER TABLE `produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `detail_pesanan`
---
-ALTER TABLE `detail_pesanan`
-  ADD CONSTRAINT `detail_pesanan_ibfk_1` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE CASCADE,
-  ADD CONSTRAINT `detail_pesanan_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
-
---
--- Ketidakleluasaan untuk tabel `pesanan`
---
-ALTER TABLE `pesanan`
-  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
